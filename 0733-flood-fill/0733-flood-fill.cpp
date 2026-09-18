c8b5a00951e227559eb5c226b6e443a9c527e5af
Jagruti Rajendra Kaulkar <139810823+jagrutikaulkar@users.noreply.github.com>
2026-09-18 15:04:15 +0530

    Time: 0 ms (100%), Space: 18.4 MB (22.27%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
+  void dfs(vector<vector<int>>& image, int x,int y, int color, int original)
+  {
+      int n=image.size();
+      int m= image[0].size();
+
+      if(x<0|| y<0 || x>=n ||y>=m|| image[x][y]!=original)
+      return;
+
+      image[x][y]=color;
+
+      dfs(image, x,y+1,color,original);
+      dfs(image,x, y-1, color, original);
+      dfs(image, x+1,y,color,original);
+      dfs(image,x-1,y,color,original);
+  }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         if (image.empty())
             return image;
+        
+        int original= image[sr][sc];
 
-        int n = image.size();
-        int m = image[0].size();
+        if(original==color)
+        return image;
 
-        int originalColor = image[sr][sc];
-
-        // Already the required color
-        if (originalColor == color)
-            return image;
-
-        queue<pair<int, int>> q;
-        q.push({sr, sc});
-
-        image[sr][sc] = color;
-
-        int xn[4] = {0, 0, 1, -1};
-        int yn[4] = {1, -1, 0, 0};
-
-        while (!q.empty())
-        {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-
-            for (int i = 0; i < 4; i++)
-            {
-                int dx = x + xn[i];
-                int dy = y + yn[i];
-
-                // Check boundary and original color
-                if (dx < 0 || dy < 0 || dx >= n || dy >= m ||
-                    image[dx][dy] != originalColor)
-                    continue;
-
-                image[dx][dy] = color;
-                q.push({dx, dy});
-            }
-        }
+        dfs(image,sr,sc,color,original);
 
         return image;
+        
     }
 };
