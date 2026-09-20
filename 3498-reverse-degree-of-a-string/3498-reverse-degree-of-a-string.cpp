0d96d3b0c9c681a38ad8263a53e83932fe90ec97
Jagruti Rajendra Kaulkar <139810823+jagrutikaulkar@users.noreply.github.com>
2026-09-20 09:45:19 +0530

    Time: 0 ms (100%), Space: 9.7 MB (78.4%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,8 +4,8 @@ public:
         int count=0;
         for(int i=0;i<s.length();i++)
         {
-            int num = ((26 -(s[i]-'a'))*(i+1));
-            count+= num;
+            count+= ((26 -(s[i]-'a'))*(i+1));
+            
             
         }
         return count;
